65364332df12c3786793a3e667fff47b42d8b29f
julia <89081314528@mail.ru>
2022-02-13 18:54:16 +0300

    lesson-2

--- src/main/java/ru/hogwarts/university/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/university/controller/FacultyController.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.university.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.university.model.Faculty;
 import ru.hogwarts.university.service.FacultyService;
@@ -30,9 +31,10 @@ public class FacultyController {
         return facultyService.updateFaculty(faculty);
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteFaculty(@PathVariable(name = "id") Long facultyId) {
+    @DeleteMapping("/{id}") // в случае успеха вернется ок, а в случае неуспеха в сервисе кидается исключение
+    public ResponseEntity deleteFaculty(@PathVariable(name = "id") Long facultyId) {
         facultyService.deleteFaculty(facultyId);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/getByColor/{color}")
