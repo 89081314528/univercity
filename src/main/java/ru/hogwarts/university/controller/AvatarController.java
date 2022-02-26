c7c69abe81eb793349bbb88be7e1363decb43c1f
julia <89081314528@mail.ru>
2022-02-26 12:53:53 +0300

    lesson5

--- src/main/java/ru/hogwarts/university/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/university/controller/AvatarController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.university.model.Avatar;
-import ru.hogwarts.university.service.AvatarService;
+import ru.hogwarts.university.service.AvatarServiceImpl;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,21 +18,21 @@ import java.nio.file.Path;
 
 @RestController
 public class AvatarController {
-    private final AvatarService avatarService;
+    private final AvatarServiceImpl avatarServiceImpl;
 
-    public AvatarController(AvatarService avatarService) {
-        this.avatarService = avatarService;
+    public AvatarController(AvatarServiceImpl avatarServiceImpl) {
+        this.avatarServiceImpl = avatarServiceImpl;
     }
 
     @PostMapping(value = "/{studentId}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity uploadAvatar(@PathVariable Long studentId, @RequestParam MultipartFile avatar) throws IOException {
-        avatarService.uploadAvatar(studentId, avatar);
+        avatarServiceImpl.uploadAvatar(studentId, avatar);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping(value = "/{studentId}/avatar-from-db")
     public ResponseEntity<byte[]> downloadAvatar(@PathVariable Long studentId) {
-        Avatar avatar = avatarService.findAvatar(studentId);
+        Avatar avatar = avatarServiceImpl.findAvatar(studentId);
         if (avatar == null) {
             return ResponseEntity.notFound().build();
         }
@@ -44,7 +44,7 @@ public class AvatarController {
 
     @GetMapping(value = "/{studentId}/avatar-from-file")
     public void downloadAvatar(@PathVariable Long studentId, HttpServletResponse response) throws IOException{
-        Avatar avatar = avatarService.findAvatar(studentId);
+        Avatar avatar = avatarServiceImpl.findAvatar(studentId);
         Path path = Path.of(avatar.getFilePath());
         try(InputStream is = Files.newInputStream(path);
             OutputStream os = response.getOutputStream();) {
---
+++ src/main/java/ru/hogwarts/university/service/AvatarServiceImpl.java
@@ -0,0 +1,63 @@
+package ru.hogwarts.university.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.university.model.Avatar;
+import ru.hogwarts.university.model.Student;
+import ru.hogwarts.university.repository.AvatarRepository;
+
+import javax.transaction.Transactional;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+@Service
+@Transactional
+public class AvatarServiceImpl implements AvatarService{
+    @Value("${path.to.avatars.folder}")
+    private String avatarsDir;
+
+    private final StudentService studentService;
+    private final AvatarRepository avatarRepository;
+
+    public AvatarServiceImpl(StudentService studentService, AvatarRepository avatarRepository) {
+        this.studentService = studentService;
+        this.avatarRepository = avatarRepository;
+    }
+
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        Student student = studentService.getStudentById(studentId);
+        Path filePath = Path.of(avatarsDir, student.getName() + "." + getExtensions(avatarFile.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent()); // создает директорию
+        Files.deleteIfExists(filePath); // удаляет файл из директории, если он там есть
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW); // создает файл в директории
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+        ) {
+            bis.transferTo(bos); // записывает в файл
+        }
+        Avatar avatar = findAvatar(studentId);
+        if (avatar == null) {
+            avatar = new Avatar();
+        }
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(avatarFile.getBytes());
+        avatarRepository.save(avatar);
+    }
+
+    public Avatar findAvatar(Long studentId) {
+        return avatarRepository.findByStudentId(studentId);
+    }
+
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+}
